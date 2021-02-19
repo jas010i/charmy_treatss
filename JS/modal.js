b17608c15c7b2add5cafe525b06f2e227eca56b0
jas010i <jas010i@users.noreply.github.com>
2021-02-19 14:55:20 -0600

    x

---
+++ JS/modal.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+function enlargeImg() {
+
+var image = document.getElementById('myImg');
+
+
+    image.style.zIndex = "10";
+    image.style.position = "absolute";
+    image.style.top = "0";
+    image.style.left = "0";
+
+}
