8808fe3fdfc453f15f0c74a224280f606a1cfefd
jas010i <jas010i@users.noreply.github.com>
2021-02-21 21:01:43 -0600

    modal update
    update for modal image for both mobile and desktop

---
+++ JS/modal.js
@@ -0,0 +1,203 @@
+
+
+
+/***************************************************/
+
+
+
+function enlargeImg() {
+
+    var popUp = document.getElementById('img1');
+    var closeBtn = document.getElementById('close-btn');
+
+
+                popUp.style.width = "90%";
+                popUp.style.marginLeft = "5%";
+
+
+                        closeBtn.style.display = "block";
+
+}
+
+function closeImage() {
+
+    var popUp = document.getElementById('img1');
+    var closeBtn = document.getElementById('close-btn');
+
+
+                popUp.style.width = "35%";
+
+
+                        closeBtn.style.display = "none";
+
+}
+
+
+
+/***************************************************/
+
+
+
+function enlargeImg2() {
+
+    var popUp = document.getElementById('img2');
+    var closeBtn = document.getElementById('close-btn2');
+
+
+                popUp.style.width = "90%";
+                popUp.style.marginLeft = "5%";
+
+
+                        closeBtn.style.display = "block";
+
+}
+
+function closeImage2() {
+
+    var popUp = document.getElementById('img2');
+    var closeBtn = document.getElementById('close-btn2');
+
+
+                popUp.style.width = "35%";
+
+
+                        closeBtn.style.display = "none";
+
+}
+
+
+
+/***************************************************/
+
+
+
+function enlargeImg3() {
+
+    var popUp = document.getElementById('img3');
+    var closeBtn = document.getElementById('close-btn3');
+
+
+                popUp.style.width = "90%";
+                popUp.style.marginLeft = "5%";
+
+
+                        closeBtn.style.display = "block";
+
+}
+
+function closeImage3() {
+
+    var popUp = document.getElementById('img3');
+    var closeBtn = document.getElementById('close-btn3');
+
+
+                popUp.style.width = "35%";
+
+
+                        closeBtn.style.display = "none";
+
+}
+
+
+
+/***************************************************/
+
+
+
+function enlargeImg4() {
+
+    var popUp = document.getElementById('img4');
+    var closeBtn = document.getElementById('close-btn4');
+
+
+                popUp.style.width = "90%";
+                popUp.style.marginLeft = "5%";
+
+
+                        closeBtn.style.display = "block";
+
+}
+
+function closeImage4() {
+
+    var popUp = document.getElementById('img4');
+    var closeBtn = document.getElementById('close-btn4');
+
+
+                popUp.style.width = "35%";
+
+
+                        closeBtn.style.display = "none";
+
+}
+
+
+
+/***************************************************/
+
+
+
+function enlargeImg5() {
+
+    var popUp = document.getElementById('img5');
+    var closeBtn = document.getElementById('close-btn5');
+
+
+                popUp.style.width = "90%";
+                popUp.style.marginLeft = "5%";
+
+
+                        closeBtn.style.display = "block";
+
+}
+
+function closeImage5() {
+
+    var popUp = document.getElementById('img5');
+    var closeBtn = document.getElementById('close-btn5');
+
+
+                popUp.style.width = "35%";
+
+
+                        closeBtn.style.display = "none";
+
+}
+
+
+
+/***************************************************/
+
+
+
+function enlargeImg6() {
+
+    var popUp = document.getElementById('img6');
+    var closeBtn = document.getElementById('close-btn6');
+
+
+                popUp.style.width = "90%";
+                popUp.style.marginLeft = "5%";
+
+
+                        closeBtn.style.display = "block";
+
+}
+
+function closeImage6() {
+
+    var popUp = document.getElementById('img6');
+    var closeBtn = document.getElementById('close-btn6');
+
+
+                popUp.style.width = "35%";
+
+
+                        closeBtn.style.display = "none";
+
+}
+
+
+
+/***************************************************/
+
