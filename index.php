7abede3e12b29c53c86e914c83a91a80c7955034
jas010i <63612124+jas010i@users.noreply.github.com>
2021-05-28 12:32:33 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,357 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>Charmy Treats - Delicious Home Made Treats and Baked Goods</title>
+        <link rel="icon" href="Images/logo_web/charmy-treats-logo.png">
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+        <!--CSS Links Here-->
+            <link href="StyleSheets/main.css" rel="stylesheet" type="text/css">
+            <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+
+        <!--Javascript Links Here-->
+            <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+
+        <!--Font links here-->
+            <link rel="preconnect" href="https://fonts.gstatic.com">
+            <link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@700&display=swap" rel="stylesheet">
+            <link href="https://fonts.googleapis.com/css2?family=Amatic+SC&display=swap" rel="stylesheet">
+
+
+        <!--Bootstrap-->
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+
+
+</head>
+
+<body style="background-color: rgba(255, 194, 204, 0.67);">
+
+    <header data-aos="fade-down" data-aos-duration="1000">
+
+        <div class="header-title">
+            <a href="index.html">
+                <img id="background" src="Images/logo_web/background-header.png" alt="logo">
+                <img id="logo" src="Images/logo_web/charmy-treats-logo.png" alt="background">
+            </a>
+        </div>
+
+            <div class="navbar1">
+
+              <a href="index.html" id="banner">Charmy Treats</a>
+
+
+                        <a class="active" href="index.html">
+                            <svg id="hashbar-right" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-shop" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.371 2.371 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976l2.61-3.045zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0zM1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5zM4 15h3v-5H4v5zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3zm3 0h-2v3h2v-3z"/>
+                            </svg>
+                            <p style="padding: 0; margin: 0;">HOME</p>
+                        </a>
+
+                        <a href="treats.html">
+                                <svg id="hashbar-right" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-cart" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+                                </svg>
+                                  <p style="padding: 0; margin: 0;">TREATS</p>
+                        </a>
+
+                        <a href="custom.html">
+                            <svg id="hashbar-right" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+                            </svg>
+                              <p style="padding: 0; margin: 0;">CUSTOM</p>
+                        </a>
+
+                        <a href="reviews.html">
+                                <svg id="hashbar-right" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-bookmark-star" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"/>
+                                    <path d="M7.84 4.1a.178.178 0 0 1 .32 0l.634 1.285a.178.178 0 0 0 .134.098l1.42.206c.145.021.204.2.098.303L9.42 6.993a.178.178 0 0 0-.051.158l.242 1.414a.178.178 0 0 1-.258.187l-1.27-.668a.178.178 0 0 0-.165 0l-1.27.668a.178.178 0 0 1-.257-.187l.242-1.414a.178.178 0 0 0-.05-.158l-1.03-1.001a.178.178 0 0 1 .098-.303l1.42-.206a.178.178 0 0 0 .134-.098L7.84 4.1z"/>
+                                </svg>
+                                  <p style="padding: 0; margin: 0;">REVIEWS</p>
+                        </a>
+                        <a id="order-now" href="treat-order-form.html">
+                          <svg id="hashbar-right"width="1em" height="1em" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-basket" viewBox="0 0 16 16">
+                              <path d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1v4.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 13.5V9a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h1.217L5.07 1.243a.5.5 0 0 1 .686-.172zM2 9v4.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V9H2zM1 7v1h14V7H1zm3 3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 4 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 6 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 8 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5z"/>
+                          </svg>
+                        <p style="padding: 0; margin: 0;">ORDER NOW</p>  
+                  </a>
+                            
+            </div>
+    </header>
+
+    <div class="welcome-body">
+
+        <h1 id="hello" data-aos="fade-left" data-aos-duration="1000">Hello! Welcome to Charmy Treats!</h1>
+
+
+
+        <div data-aos="fade-left" data-aos-duration="1000" class="main-weekly-updates">
+            <h1>Weekly updates</h1>
+
+                <p>Our March menu is up now, and we are now taking special<br>'St. Patty' orders for the month of march!</p>
+
+                <div data-aos="fade-up" data-aos-duration="1000" id="myCarousel" class="carousel slide" data-ride="carousel">
+                    <h1 
+                    style="font-family: 'Amatic SC', sans-serif;top: 0; width: 100%; position: absolute; z-index: +3; background-color: rgba(0, 0, 0, 0.205); color: rgb(255, 255, 255); font-weight: lighter; padding-left: 2%; padding-top: 2%; padding-bottom: 2%; margin: 0;">
+                    Treats of the Week
+                    </h1>
+              
+                      
+                      
+              
+                      <!-- Indicators -->
+                      <ol class="carousel-indicators">
+                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#myCarousel" data-slide-to="1"></li>
+                        <li data-target="#myCarousel" data-slide-to="2"></li>
+                        <li data-target="#myCarousel" data-slide-to="3"></li>
+                      </ol>
+              
+              
+                     
+                    
+                      <!-- Wrapper for slides -->
+                      <div class="carousel-inner">
+                        <div class="item active">
+                          <img style="width: 100%;" src="Images/treats_page/treats-cake-pop-1.jpeg" alt="Cake Pop">
+                          <div class="carousel-caption">
+                            <p>Cake Pop's</p>
+                          </div>
+                        </div>
+                    
+                        <div class="item">
+                          <img style="width: 100%;" src="Images/treats_page/treats-chocolate-straberry-cake-1.jpeg" alt="Chocolate Strawberry Cake">
+                          <div class="carousel-caption">
+                            <p>Chocolate Cake w/ Real Strawberries</p>
+                          </div>
+                        </div>
+                    
+                        <div class="item">
+                          <img style="width: 100%;" src="Images/treats_page/treats-oreo-bites.jpg" alt="Oreo Bites">
+                          <div class="carousel-caption">
+                            <p>Oreo Bites</p>
+                          </div>
+                        </div>
+              
+                        <div class="item">
+                          <img style="width: 100%;" src="Images/treats_page/treats-pie-1.jpeg" alt="Pie">
+                          <div class="carousel-caption">
+                            <p>Blueberry Pie</p>
+                          </div>
+                        </div>
+                      </div>
+                    
+                      <!-- Left and right controls -->
+                      <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left"></span>
+                        <span class="sr-only">Previous</span>
+                      </a>
+                      <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right"></span>
+                        <span class="sr-only">Next</span>
+                      </a>
+                    </div>
+        
+
+
+        </div>
+
+
+
+      
+        <div class="month-treat">
+
+
+          <div div data-aos="fade-left" data-aos-duration="1000"  class="month-menu">
+            <img src="Images/monthly_menu/marchMenu/marchMenu_2021.jpg" alt="March Menu">
+          </div>
+
+          <div div data-aos="fade-left" data-aos-duration="1000"  class="month-price">
+            <img src="Images/monthly_menu/marchMenu/marchPrices_2021.jpg" alt="March Menu Prices">
+          </div>
+
+
+
+
+
+        </div>
+
+
+
+
+
+
+<div data-aos="fade-left" data-aos-duration="1000" class="main-treat">
+
+        
+        <h3 data-aos="fade-left" data-aos-duration="1000">Treat of the Month</h3>
+
+            <p data-aos="fade-left" data-aos-duration="1300" id="treat-month">Oreo Bites</p>
+                <p data-aos="fade-left" data-aos-duration="1300" id="treat-month" style="background-color: rgb(255, 110, 134);">Price: 10 for $10 / 20 for $20</p>
+                <div id="treat-month-img">
+                <img data-aos="fade-left" data-aos-duration="1300" src="Images/treats_page/treats-oreo-bites.jpg" alt="oreo bites">
+                </div>
+            <h3 data-aos="fade-right" data-aos-duration="1000" style="margin-top: 0;">Top Picks</h3>
+
+                <ul data-aos="fade-right" data-aos-duration="1300">
+                    <li>Strawberry Cheesecake</li>
+                    <li>Chocolate Bundt Cake topped w/ Real Strawberries</li>
+                    <li>Blueberry Pie</li>
+                    <li>Apple Pie</li>
+                </ul>
+
+
+                </div>
+                
+                <a data-aos="fade-left" data-aos-duration="1000" class="treat-form-link" href="treat-order-form.html">
+                    Already know what you want to order?</a>
+
+    </div>
+
+
+
+
+
+
+    <div class="newsletter">
+
+<style>
+  .news-form {
+    width: 100%;
+  }
+
+  .news-form {
+    width: 50%;
+    margin-left: 25%;
+  }
+
+  .news-form > form > h1 {
+    font-family: 'Amatic SC', cursive;
+    font-size: 3vw;
+    font-weight: 900;
+    text-align: center;
+    margin-top: 15%;
+  }
+
+  .news-form > form > input {
+    width: 100%;
+    background-color: rgba(0, 0, 0, 0);
+    padding: 2%;
+    border: none;
+    border-bottom: 1px solid rgb(248, 16, 55);
+    transition: .5s ease-in-out;
+    margin-bottom: 5%;
+    font-family: 'Amatic SC', cursive;
+    font-size: 2vw;
+  }
+
+  .news-form > form > button {
+    width: 100%;
+    background-color: rgba(0, 0, 0, 0);
+    padding: 2%;
+    border: none;
+    border-bottom: 1px solid rgb(248, 16, 55);
+    transition: .5s ease-in-out;
+    margin-bottom: 5%;
+    font-family: 'Amatic SC', cursive;
+    font-size: 2vw;
+  }
+
+  @media only screen and (max-width: 700px) {
+    
+    .news-form {
+      width: 80%;
+      margin-left: 10%;
+    }
+
+    .news-form > form > h1 {
+      margin-top: 25%;
+      font-size: 7vw;
+    }
+
+    .news-form > form > input {
+      margin-bottom: 35%;
+      font-size: 5vw;
+    }
+
+    .news-form > form > button {
+      margin-bottom: 35%;
+      font-size: 5vw;
+    }
+
+  }
+</style>
+
+
+
+
+        <div class="news-form">
+
+            <form action="PHP/newsletter_signup.php" method="POST">
+            
+              <h1>Subscribe to our newsletter for the latest updates!</h1>
+
+                <input style="margin-bottom: 5%;" id="email" name="email" type="email" placeholder="Your email" required>
+
+                <button style="border-radius: 6px; background-color: rgb(255, 49, 83); border: 2px solid white; color: white;" id="submit" name="submit">Subscribe Now</button>
+            
+            </form>
+
+        </div>
+
+
+
+
+    </div>
+
+
+
+
+
+    <footer>
+
+        <img src="Images/logo_web/background-footer.png" alt="footer">
+    
+    
+    
+        <div class="footer-list">
+    
+
+        <a href="contact.html">Contact Us</a>
+            <br><br>
+    
+        </div>
+    
+        <div class="footer-list2">
+    
+          <a href="reviews.html">Leave a Review</a>
+          <br><br>
+            <a href="notice.html">Notice about Ingredients</a>
+    
+        </div>
+    
+            <p id="copyright">Published and Managed by iridescXnt designs.
+                                <br>
+                                Content owned by Charmy Treats. Established 2020. All Rights Reserved.
+            </p>
+    
+    </footer>
+    
+    
+    <script>
+        AOS.init();
+    </script>
+    
+    </body>
+    
+    
+    </html>
